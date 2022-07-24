24b069f0f6e839909e8bce5a8ec742f04d44b8b8
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-07-24 10:47:36 +0200

    Add Treasury

---
+++ contracts/Treasury.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Treasury is Ownable {
+
+    uint256 public immutable totalFunds;
+
+    address public immutable payee;
+    
+    bool public isReleased;
+
+    constructor(address _payee) payable {
+        totalFunds = msg.value;
+        payee = _payee;
+        isReleased = false;
+    }
+
+    function releaseFunds() public onlyOwner {
+        isReleased = true;
+        payable(payee).transfer(totalFunds);
+    }
+}
